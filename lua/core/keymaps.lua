4d0b655c2804b8946561ae1d0eef97e5580284f3
Zawa <155267500+rqms40@users.noreply.github.com>
2024-09-30 15:40:24 +0800

    feat: add new remaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -77,3 +77,8 @@ vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open float
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 vim.keymap.set("i", "jj", "<Esc>", opts)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
