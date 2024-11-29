88df6b0440bdea85e357258b177be8ef359fcb79
Zawa <155267500+rqms40@users.noreply.github.com>
2024-11-29 19:04:41 +0800

    feat(telescope): add 'jj' for exiting

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -53,6 +53,7 @@ return {
 						["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
 						["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
 						["<C-l>"] = require("telescope.actions").select_default, -- open file
+						["jj"] = require("telescope.actions").close,
 					},
 				},
 			},
